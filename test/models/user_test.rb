5b570c85e30e33510f25285e143d4f2c8e0b00d1
offorma <offorma@gmail.com>
2019-06-13 10:32:45 +0100

    added  migration for user paswword digest field

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -27,4 +27,18 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+  
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end
