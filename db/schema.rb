a1646406a0a24bc90b7eec4be3c5736f140a3285
offorma <offorma@gmail.com>
2019-06-13 03:55:24 +0100

    added some tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_12_232918) do
+ActiveRecord::Schema.define(version: 2019_06_13_024637) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
